7470e1fdba0351fa6273f6f342adb7b536fd8d0d
kmm2459358 <kmm2459358@stu.o-hara.ac.jp>
2024-07-22 12:02:27 +0900

    テトリス、完成

---
+++ FallingBlockPuzzle/FallingBlockPuzzle/BlockShape.h
@@ -0,0 +1,31 @@
+//======================================
+//      落ち物バズル ブロック形状
+//======================================
+// ★ここにインクルードガード(開始)を記入してください
+//
+#ifndef __BLOCKSHAPE_H
+#define __BLOCKSHAPE_H
+
+const int SHAPE_WID_MAX = 4;
+const int SHAPE_HEI_MAX = 4;
+
+typedef struct {
+	int size;
+	bool pattern[SHAPE_HEI_MAX][SHAPE_WID_MAX];
+} BlockShape;
+
+extern const int blockShapesSize;
+
+// shapeセット
+void SetShape(BlockShape* shape, int idx);
+// shapeを回転(反時計回り90度)
+void RotateShape(BlockShape* shape);
+// shapeパターンを設定
+void SetShapePattern(BlockShape* shape, int x, int y, bool value);
+// shapeパターンを取得
+bool GetShapePattern(BlockShape* shape, int x, int y);
+// shapeを表示
+void PrintShape(BlockShape* shape);
+
+// ★ここにインクルードガード(終了)を記入してください
+#endif
