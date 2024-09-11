f24ade807d787c1c0cda34c3d6b90640445aeffe
kmm2459358 <kmm2459358@stu.o-hara.ac.jp>
2024-09-11 11:47:31 +0900

    王道RPG完全版

---
+++ ClassicRPG/ClassicRPG/UI.h
@@ -0,0 +1,14 @@
+//======================================
+//	コマンドUI
+//======================================
+#ifndef __UI_H
+#define __UI_H
+#include "Command.h"
+#include "TurnBattle.h"
+
+// プレーヤのコマンド取得
+Command GetPlayerCommand(TurnBattle* btl);
+// 敵のコマンド取得
+Command GetEnemyCommand();
+
+#endif __UI_H
